8011a6fc9952ea108d92615a4c149ff0a10aac46
Nistha <nistha984475@gmail.com>
2024-09-13 12:20:15 +0545

    adds eslint rules

---
+++ lint/eslint.config.mjs
@@ -0,0 +1,35 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import tsParser from "@typescript-eslint/parser";
+
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: globals.browser, parser: tsParser,parserOptions: {
+    project: './tsconfig.json',  
+  },}},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  {rules: {
+    "no-console": "warn", 
+    "semi": ["error", "always"], 
+    "@typescript-eslint/no-unused-vars": "error", 
+    "complexity": ["error", 2],
+     "@typescript-eslint/no-unnecessary-condition": "error",
+     "@typescript-eslint/no-floating-promises": "error",
+     "no-shadow": "off",
+    "@typescript-eslint/no-shadow": "error",
+    // "@typescript-eslint/naming-convention": ["warn", 
+    //   {
+    //     selector: 'variable',
+    //     format: ['camelCase'],
+    //     leadingUnderscore: 'allow',
+    //     trailingUnderscore: 'allow',
+    //   },
+    // ]
+  },
+}
+];
---
+++ lint/src/index.ts
@@ -0,0 +1,30 @@
+console.log("hello world!");
+let variable_naming;
+function check_cyclomatic_complexity(x:number){
+    if (x>0) {
+        return x; 
+    }  else {
+        return 4; 
+    }
+}
+
+// check_cyclomatic_complexity(2);
+
+
+// No unnecessary condition
+// function check(){
+//     if (true) return 1;
+//     else return 0;
+// }
+
+// async function check_promise(){
+//     const promise = new Promise((resolve, reject) => resolve('value'));
+//     promise;
+// }
+
+//shadow error
+// const nameString = 'abc';
+// const change = () =>{
+//     const nameString = 'xyz';
+//     console.log(nameString);
+// };
