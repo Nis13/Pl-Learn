52dc890a4f9503578bab57ed0e2ed7fb772df25a
Nistha <nistha984475@gmail.com>
2024-09-16 09:09:29 +0545

    adds return type to functions

--- CRUD/src/services/user.service.ts
+++ CRUD/src/services/user.service.ts
@@ -1,22 +1,23 @@
 import { User } from "../interface/user.interface";
 import { createUser, deleteUserById, getAllUsersModel, getUserById, updateUser } from "../model/model.user";
 
-export function getAllUsersService(){
+export function getAllUsersService(): User[]{
     return getAllUsersModel();
 }
 
-export function getUserByIdService(id:number){
+export function getUserByIdService(id:number): User | undefined{
     return getUserById(id);
 }
 
-export function createUserService(userDetail: Pick<User, 'name'|'email'>){
+export function createUserService(userDetail: Pick<User, 'name'|'email'>): string{
     return createUser(userDetail);
 }
 
-export function updateUserService(id:number,userDetail: Pick<User, 'name'|'email'>){
+export function updateUserService(id:number,userDetail: Pick<User, 'name'|'email'>): User | undefined {
+    getUserById(id);
     return updateUser(id,userDetail);
 }
 
-export function deleteUserService(id:number){
+export function deleteUserService(id:number): string{
     return deleteUserById(id);
 }
