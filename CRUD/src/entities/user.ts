7f9b89ab1ac4c3c9d3407e5f0d4ef13b03213a42
Nistha <nistha984475@gmail.com>
2024-09-17 13:57:19 +0545

    refactor: update entity fields

--- CRUD/src/entities/user.ts
+++ CRUD/src/entities/user.ts
@@ -1,13 +1,19 @@
-import {  Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import {  Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm";
 
 @Entity()
 export class User{
-    @PrimaryGeneratedColumn()
-    id:number;
+    @PrimaryColumn("uuid")
+    id: string;
 
-    @Column("varchar",{length:200})
-    name:string;
+    @Column("varchar",{length: 200})
+    name: string;
 
-    @Column("varchar",{length:200})
-    email:string;
+    @Column("varchar",{length: 200})
+    email: string;
+
+    @CreateDateColumn({ type: 'timestamp' })
+    createdAt:Date;
+    
+    @UpdateDateColumn({ type: 'timestamp' })
+    updatedAt:Date;
 }
