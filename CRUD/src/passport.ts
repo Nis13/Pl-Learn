fd9713ab41c6d9750e4e8d5b6034046f0d0cc529
Nistha <nistha984475@gmail.com>
2024-09-25 12:37:37 +0545

    update passport.js to return the payload

--- CRUD/src/passport.ts
+++ CRUD/src/passport.ts
@@ -1,6 +1,5 @@
 import { ExtractJwt, Strategy } from "passport-jwt";
 import passport from "passport";
-import * as UserService from "./repository/user.repo";
 import config from "./config";
 
 const opts = {
@@ -10,12 +9,7 @@ const opts = {
 
 passport.use(
   new Strategy(opts, async (payload, done) => {
-    try {
-      const user = await UserService.getById(payload.id);
-      if (user) return done(null, user);
-    } catch (error) {
-      return done(error);
-    }
+    return done(null, payload);
   })
 );
 
