49a8c617b36aa9413d7e158d8e00556ead2ea9b3
Nistha <nistha984475@gmail.com>
2024-09-25 07:21:49 +0545

    extend the updateProductDTO

--- CRUD/src/DTO/updateProduct.dto.ts
+++ CRUD/src/DTO/updateProduct.dto.ts
@@ -1,11 +1,5 @@
-import {
-  IsNumber,
-  IsString,
-  Length,
-  IsPositive,
-  IsOptional,
-  IsArray,
-} from "class-validator";
+import { IsOptional } from "class-validator";
+import { CreateProductDTO } from "./createProduct.dto";
 
 /**
  * @openapi
@@ -32,18 +26,13 @@ import {
  *           example: ["67e27f08-7709-4d04-ae36-76f85bdf888c"]
  */
 
-export class UpdateProductDTO {
+export class UpdateProductDTO extends CreateProductDTO {
   @IsOptional()
-  @IsString()
-  @Length(1, 200, { message: "Name must be between 1 and 200 characters" })
-  name?: string;
+  name: string;
 
   @IsOptional()
-  @IsNumber({}, { message: "Price must be a number" })
-  @IsPositive({ message: "Price must be a positive number" })
-  price?: number;
+  price: number;
 
   @IsOptional()
-  @IsArray()
   category?: string[];
 }
